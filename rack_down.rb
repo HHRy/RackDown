fe0cf869d0e25c8b7f35b06163ceac90af22cd28
Ryan Stenhouse <ryan@ryanstenhouse.eu>
2011-04-15 22:32:27 +0100

    Simplistic routing

--- rack_down.rb
+++ rack_down.rb
@@ -1,14 +1,36 @@
 module RackDown
   class Application
     def call(env)
-      root = File.expand_path(File.dirname(__FILE__) + '/posts')
-      if env['REQUEST_PATH'] == '/'
-        [200, { 'Content-Type' => 'text/html' }, RDiscount.new(IO.read(root + '/index.md')).to_html]
-      elsif File.exists?(root + env['REQUEST_PATH'] + '.md')
-        [200, { 'Content-Type' => 'text/html' }, RDiscount.new(IO.read(root + env['REQUEST_PATH'] + '.md')).to_html]
+      @router = Router.new(File.expand_path(File.dirname(__FILE__) + '/posts'))
+      @router.route(env['REQUEST_PATH'])
+    end
+  end
+  
+  class Router
+    
+    attr_reader :root, :index
+    
+    def initialize(root)
+      @root = root
+      @index = root + '/index.md'
+      @not_found = root + '/404.md'
+      build_routing_hash
+    end
+    
+    def route(request_path)
+      if request_path == '/'
+        [200, { 'Content-Type' => 'text/html' }, RDiscount.new(IO.read(@index)).to_html]
+      elsif @allowed_files.include?(@root + request_path + '.md')
+        [200, { 'Content-Type' => 'text/html' }, RDiscount.new(IO.read(@root + request_path + '.md')).to_html]
       else
-        [404, { 'Content-Type' => 'text/html' }, RDiscount.new(IO.read(root + '/404.md')).to_html]
+        [404, { 'Content-Type' => 'text/html' }, RDiscount.new(IO.read(@not_found)).to_html]
       end
+    end
+    
+    private 
+    
+    def build_routing_hash
+      @allowed_files = (Dir.glob(@root + '/*.md') + Dir.glob(@root + '/*.mdown') + Dir.glob(@root + '/*.test'))
     end
   end
 end
